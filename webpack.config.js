856297e528b9e1ea09186c5d9cfb82452e754cbe
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-08-03 00:26:20 +0530

    setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: { index: path.resolve(__dirname, 'src', 'index.js') },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'src'),
+  },
+};
