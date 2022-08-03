1f4424e8e892654b1bb6fa44af16b3e9bf295e44
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-08-03 15:08:00 +0530

    updated webpack.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 /* eslint-disable prefer-const */
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
-import "./style.css";
+import _ from 'lodash';
+import './style.css';
+
 const apiKey = '54a6527497256dd9bae8275602a3260a';
 let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=${apiKey}`;
 
 window.addEventListener('load', () => {
 
-
 });
+
