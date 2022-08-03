2537eafaabe700f0469ceb295cd87ba6af5e6bd6
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-03 16:42:58 +0300

    Update configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,20 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: { index: path.resolve(__dirname, 'src', 'index.js') },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
-  ],
-  output: {
-    path: path.resolve(__dirname, 'src'),
-  },
-  rules: [
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: ['babel-loader'],
-    },
   ],
 };
