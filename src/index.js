27013c191a654e93ef45e60d5330814662002d06
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-08-03 10:38:00 +0530

    API set

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable prefer-const */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+const apiKey = '54a6527497256dd9bae8275602a3260a';
+let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=${apiKey}`;
+
+window.addEventListener('load', () => {
+
+});
