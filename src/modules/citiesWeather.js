6d3cf3c4782ca0a389b6c6b0050cf51fa95ec558
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-05 01:37:03 +0300

    Update citiesWeather to contain popup window

--- src/modules/citiesWeather.js
+++ src/modules/citiesWeather.js
@@ -1,7 +1,14 @@
 import img from '../images/heart.png';
+import popUp from './commentPopup.js';
 
 const apiKey = '54a6527497256dd9bae8275602a3260a';
 const parent = document.getElementById('main-content');
+const btn = document.getElementsByClassName('btn');
+const popInfo = document.querySelector('.popUp');
+const header = document.querySelector('.searchContainer');
+const main = document.querySelector('.main-content');
+const footer = document.querySelector('.footerContainer');
+
 const otherCitiesWeather = async (city) => {
   const container = document.createElement('div');
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -26,9 +33,21 @@ const otherCitiesWeather = async (city) => {
       list.innerHTML += `   <li class="weatherinfo">Degree: ${temp}</li>
                             <li class="weatherinfo">Weather: ${description}</li> `;
       container.append(list);
-      container.innerHTML += '<button type="button" class="bg-success btn">Comment</button>';
+      container.innerHTML += `<button type="button" id=${data.name} class="bg-success btn">Comment</button>`;
     });
   parent.append(container);
+
+  for (let i = 0; i < btn.length; i += 1) {
+    btn[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      const id = btn[i].getAttribute('id');
+      popUp(id);
+      header.classList.add('hidden');
+      main.classList.add('hidden');
+      footer.classList.add('hidden');
+      popInfo.classList.remove('hidden');
+    });
+  }
 };
 
 export default otherCitiesWeather;
