9f3ae4677d2e37cd23563bee101d523a191a8d23
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-08-06 21:54:43 +0530

    updated code for recording/displaying likes

--- src/modules/citiesWeather.js
+++ src/modules/citiesWeather.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-vars */
 import img from '../images/heart.png';
 import popUp from './commentPopup.js';
+import { Displaylikes, likeapi } from './likes.js';
 
 const apiKey = '54a6527497256dd9bae8275602a3260a';
 const likesUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/cuyuahQqhc46iVj8wwV2/likes';
@@ -33,41 +34,52 @@ const otherCitiesWeather = async (city) => {
       const { description, icon } = data.weather[0];
       const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
       // const fahrenheit = (temp * 9) / 5 + 32;
+      container.setAttribute('class', 'city-weather' )
+      container.innerHTML = ` 
+                   <div id="icondiv"><img id="imgdesc" src=${iconUrl}></div>
+                    <div class="likediv">
+                        <span class="info">${place}</span>
+                        <img src=${img} class="likeimg"  id="likeimgs" data=${data.name}>
+                     </div>
+         <ul class="'currentinfo'">
+              <li class="weatherinfo">Degree: ${temp}</li>
+              <li class="weatherinfo">Weather: ${description}</li> 
+              <li class="likes-item"><span class= "likes-qty" data="${data.name}">0</span> likes</li>
+          </ul>
+          
+          <button type="button" id=${data.name} class="bg-success btn">Comment</button>
+        </div>
+      </div> `;
+        parent.appendChild(container);
 
-      container.setAttribute('id', 'city-weather');
-      container.innerHTML = `<div id="icondiv"><img id="imgdesc" src=${iconUrl}></div>`;
-      const titlediv = document.createElement('div');
-      titlediv.setAttribute('class', 'likediv');
-      titlediv.innerHTML = `<span class="info">  ${place}</span>;
-                            <img src=${img} class="likeimg"  id="likeimg">
-                            <div class="likes" id="likes"><span class="like" id="like">${nolikes} likes</span></div>`;
-      container.append(titlediv);
-      const list = document.createElement('ul');
-      list.setAttribute('class', 'currentinfo');
-      list.innerHTML += `   <li class="weatherinfo">Degree: ${temp}</li>
-                            <li class="weatherinfo">Weather: ${description}</li> `;
-      container.append(list);
-      container.innerHTML += `<button type="button" id=${data.name} class="bg-success btn">Comment</button>`;
+      
     });
-  parent.append(container);
-  const heartBtn = document.getElementsByClassName('likeimg');
-  for (let i = 0; i < heartBtn.length; i += 1) {
-    heartBtn[i].addEventListener('click', async (e) => {
-      e.stopPropagation();
-      const response = await fetch(likesUrl, {
-        method: 'POST',
-        body: JSON.stringify({
-          item_id: Math.floor(Math.random(1) * 1000),
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      }).then((response) => {
-        getLikes(likesUrl);
-        window.location.reload();
-        return response.text();
+    for (let i = 0; i < btn.length; i += 1) {
+      btn[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        const id = btn[i].getAttribute('id');
+        popUp(id);
+        header.classList.add('hidden');
+        main.classList.add('hidden');
+        footer.classList.add('hidden');
+        popInfo.classList.remove('hidden');
       });
-    });
-  }
+    }
+  
+    const like = () => {
+      const likebtns = document.getElementsByClassName('likeimg');
+      const likebtnsarray = Array.from(likebtns);
+      likebtnsarray.forEach((element) => {
+        const likeId = element.getAttribute('data');
+        element.addEventListener('click', () => {
+          element.classList.add('heart-active');
+          likeapi(likeId);
+        });
+      });
+    };
+    
+    setTimeout(() => like(), 1000);
+    Displaylikes();
+  
 };
 export default otherCitiesWeather;
