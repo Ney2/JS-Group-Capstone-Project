7d60f00c10f6af9cef9d1905c0c1bdc7c04d3358
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-08-06 23:18:16 +0530

    fixed linters

--- src/Test/likes.test.js
+++ src/Test/likes.test.js
@@ -1,33 +1,31 @@
-
 /**
  * @jest-environment jsdom
  */
 
- import { apiMock2, countertests } from '../_mocks_/likes.js';
+import { apiMock2, countertests } from '../_mocks_/likes.js';
 
- describe('Add a new city', () => {
-   test('Add a new city', () => {
-     expect(apiMock2(3, 'city').length).toBe(3);
-   });
- 
-   test('Get number of city', () => {
-     const cities = [{
-       id: '1',
-       name: 'Madrid',
-     }, {
-       id: '2',
-       name: 'Berlin',
-     },
-     {
-       id: '3',
-       name: 'Sydney',
-     },
-     {
-       id: '4',
-       name: 'Tokyo',
-     },
-     ];
-     expect(countertests(cities).length).toBe(4);
-   });
- });
- 
+describe('Add a new city', () => {
+  test('Add a new city', () => {
+    expect(apiMock2(3, 'city').length).toBe(3);
+  });
+
+  test('Get number of city', () => {
+    const cities = [{
+      id: '1',
+      name: 'Madrid',
+    }, {
+      id: '2',
+      name: 'Berlin',
+    },
+    {
+      id: '3',
+      name: 'Sydney',
+    },
+    {
+      id: '4',
+      name: 'Tokyo',
+    },
+    ];
+    expect(countertests(cities).length).toBe(4);
+  });
+});
